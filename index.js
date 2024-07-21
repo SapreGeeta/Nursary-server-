e46159eaf4fc318a38a3888a39be2d154acd1d3a
SapreGeeta <gitasapre@gmail.com>
2024-07-21 15:25:23 +0530

    database connection and server is connected

---
+++ index.js
@@ -0,0 +1,49 @@
+import express, { json } from "express";
+import dotenv, { config } from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+import cors from "cors"
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+import { getHealth } from "./controllers/health.js";
+import { handlePageNotFound} from "./controllers/error.js";
+import {
+  postPlant,
+  getPlants,
+  getPlantId,
+  putPlantId,
+  deletePlantId,
+} from "./controllers/plant.js";
+
+const dbConnection = async ()=>{
+ const conn = await mongoose.connect(process.env.MONGO_URL)
+
+ if(conn){
+  console.log(`Connected to DB 👍🏻`)
+ }
+ else{
+  console.log(`Error connecting to DB 😢`)
+ }
+}
+
+dbConnection()
+
+
+
+app.get("/health", getHealth);
+app.post("/plant", postPlant);
+app.get("/plants", getPlants);
+app.get("/plant/:id", getPlantId);
+app.put("/plant/:id", putPlantId);
+app.delete("/plant/:id" , deletePlantId);
+
+app.use("*" , handlePageNotFound )
+
+const PORT = 6000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
