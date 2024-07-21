b3ade54d18347b8e86553a2e88dc58a11d0ec2a7
SapreGeeta <gitasapre@gmail.com>
2024-07-21 15:28:44 +0530

    controllers added

---
+++ controllers/plant.js
@@ -0,0 +1,91 @@
+import { Schema } from "mongoose";
+import Plant from "../models/Plant.js";
+
+const updateDescriptions = async () => {
+  await mongoose.connect("mongodb://localhost:27017/yourdatabase");
+
+  console.log("Updated all documents to use 'description' instead of 'discription'");
+  mongoose.disconnect();
+};
+
+
+const postPlant = async (req, res) => {
+  const { name, category, image, price} = req.body; 
+
+  const newPlant = new Plant({
+    name: name,
+    category: category,
+    image: image,
+    price: price,
+  });
+
+  const savedPlant = await newPlant.save();
+
+  res.json({
+    message: "plant added successfully",
+    success: true,
+    data: savedPlant,
+  });
+};
+
+const getPlants = async (req, res) => {
+    const allPlant = await Plant.find().sort({updatedAt: -1})
+  res.json({
+    message: "plants fetched successfully",
+    success: true,
+    data: allPlant,
+  });
+};
+
+const getPlantId = async (req, res) => {
+  const { id } = req.params;
+
+  const plant = await Plant.findById(id)
+
+  res.json({
+    message: "plant fetched successfully",
+    success: true,
+    data: plant,
+  });
+};
+
+const putPlantId = async(req, res) => {
+  const {
+     name,
+     category,
+     image,
+     price,
+     description,
+  } = req.body;
+
+  const { id } = req.params;
+  const updateResult = await Plant.updateOne({_id : id},{
+    $set: {
+        name: name ,
+        category: category,
+        image: image,
+        price: price,
+    }})
+     const updatedPlant = await Plant.findById(id)
+
+    res.json({
+        message: "plant updated successfully",
+        success: true,
+        data : updatedPlant
+    })
+}
+
+const deletePlantId = async (req, res) => {
+  const { id } = req.params;
+ await Plant.deleteOne({
+    _id : id 
+ })
+
+  res.json({
+    message: "plant deleted successfully",
+    success: true,
+    data: null,
+  });
+};
+
+export { postPlant, getPlants, getPlantId, putPlantId, deletePlantId};
